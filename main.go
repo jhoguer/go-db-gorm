bc22e4545e86cd6619d186552901fa1e13f49206
Jhon Guerrero <jhoguer@gmail.com>
2020-10-05 22:10:08 -0500

    Declarando modelos

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/jhoguer/go-db-gorm/storage"
 
 func main() {
-	driver := storage.MySQL
+	driver := storage.Postgres
 	storage.New(driver)
 	storage.New(driver)
 	storage.New(driver)
