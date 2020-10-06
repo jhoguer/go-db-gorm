e4151e37f265015b29474549c4b9e4d53ec26b08
Jhon Guerrero <jhoguer@gmail.com>
2020-10-05 22:34:28 -0500

    Creando migraciones

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-import "github.com/jhoguer/go-db-gorm/storage"
+import (
+	"github.com/jhoguer/go-db-gorm/model"
+	"github.com/jhoguer/go-db-gorm/storage"
+)
 
 func main() {
-	driver := storage.Postgres
+	driver := storage.MySQL
 	storage.New(driver)
-	storage.New(driver)
-	storage.New(driver)
-	storage.New(driver)
-	storage.New(driver)
-	storage.New(driver)
+
+	storage.DB().AutoMigrate(
+		&model.Product{},
+		&model.InvoiceHeader{},
+		&model.InvoiceItem{},
+	)
 }
