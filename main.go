4ab7afff940b0e0e93bce92c58371fae086dc1f1
Jhon Guerrero <jhoguer@gmail.com>
2020-10-06 22:47:00 -0500

    Letendo todos los regsitros de la tabla productos

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/jhoguer/go-db-gorm/model"
 	"github.com/jhoguer/go-db-gorm/storage"
 )
 
 func main() {
-	driver := storage.Postgres
+	driver := storage.MySQL
 	storage.New(driver)
 
 	// Creando migraciones
@@ -16,24 +18,33 @@ func main() {
 	// 	&model.InvoiceItem{},
 	// )
 
-	product1 := model.Product{
-		Name:  "Curso de Go",
-		Price: 120,
-	}
+	// Creando registros
+	// product1 := model.Product{
+	// 	Name:  "Curso de Go",
+	// 	Price: 120,
+	// }
 
-	obs := "Testing with Go"
-	product2 := model.Product{
-		Name:         "Curso de Testing",
-		Price:        150,
-		Observations: &obs,
-	}
+	// obs := "Testing with Go"
+	// product2 := model.Product{
+	// 	Name:         "Curso de Testing",
+	// 	Price:        150,
+	// 	Observations: &obs,
+	// }
 
-	product3 := model.Product{
-		Name:  "Curso de Python",
-		Price: 200,
-	}
+	// product3 := model.Product{
+	// 	Name:  "Curso de Python",
+	// 	Price: 200,
+	// }
 
-	storage.DB().Create(&product1)
-	storage.DB().Create(&product2)
-	storage.DB().Create(&product3)
+	// storage.DB().Create(&product1)
+	// storage.DB().Create(&product2)
+	// storage.DB().Create(&product3)
+
+	// Leyendo multiples regsitros
+	products := make([]model.Product, 0)
+	storage.DB().Find(&products)
+
+	for _, product := range products {
+		fmt.Printf("%d - %s\n", product.ID, product.Name)
+	}
 }
