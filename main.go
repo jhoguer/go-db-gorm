67516ebb61e0ecbeaaabb581fc82a0912d91ded9
Jhon Guerrero <jhoguer@gmail.com>
2020-10-07 17:05:10 -0500

    Creando transacciones

--- main.go
+++ main.go
@@ -1,83 +1,25 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/jhoguer/go-db-gorm/model"
 	"github.com/jhoguer/go-db-gorm/storage"
 )
 
 func main() {
-	driver := storage.MySQL
+	driver := storage.Postgres
 	storage.New(driver)
 
-	// Creando migraciones
-	// storage.DB().AutoMigrate(
-	// 	&model.Product{},
-	// 	&model.InvoiceHeader{},
-	// 	&model.InvoiceItem{},
-	// )
+	// storage.DB().Model(&model.InvoiceItem{}).AddForeignKey("product_id", "products(id)", "RESTRICT", "RESTRICT")
 
-	// Creando registros
-	// product1 := model.Product{
-	// 	Name:  "Curso de Go",
-	// 	Price: 120,
-	// }
+	// storage.DB().Model(&model.InvoiceItem{}).AddForeignKey("invoice_header_id", "invoice_headers(id)", "RESTRICT", "RESTRICT")
 
-	// obs := "Testing with Go"
-	// product2 := model.Product{
-	// 	Name:         "Curso de Testing",
-	// 	Price:        150,
-	// 	Observations: &obs,
-	// }
-
-	// product3 := model.Product{
-	// 	Name:  "Curso de Python",
-	// 	Price: 200,
-	// }
-
-	// storage.DB().Create(&product1)
-	// storage.DB().Create(&product2)
-	// storage.DB().Create(&product3)
-
-	// Leyendo multiples regsitros
-	products := make([]model.Product, 0)
-	storage.DB().Find(&products)
-
-	for _, product := range products {
-		fmt.Printf("%d - %s\n", product.ID, product.Name)
+	invoice := model.InvoiceHeader{
+		Client: "Alvaro Felipe",
+		InvoiceItems: []model.InvoiceItem{
+			{ProductID: 1},
+			{ProductID: 2},
+		},
 	}
 
-	// Leyendo un unico registro
-	myProduct := model.Product{}
-
-	storage.DB().First(&myProduct, 3)
-	fmt.Println(myProduct)
-
-	// Actualizando un resgitro parcialmente
-	myProduct.ID = 3
-
-	storage.DB().Model(&myProduct).Updates(
-		model.Product{Name: "Curso de Testing con Jest", Price: 130},
-	)
-
-	storage.DB().First(&myProduct, 3)
-	fmt.Println(myProduct)
-
-	// Realizando borrado suave
-	// myProduct.ID = 3
-
-	// storage.DB().Delete(&myProduct)
-
-	// storage.DB().Find(&products)
-
-	// for _, product := range products {
-	// 	fmt.Printf("%d - %s\n", product.ID, product.Name)
-	// }
-
-	// Borrado permanente
-	myProduct = model.Product{}
-	myProduct.ID = 3
-
-	storage.DB().Unscoped().Delete(&myProduct)
+	storage.DB().Create(&invoice)
 }
